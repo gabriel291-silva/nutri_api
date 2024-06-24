06f166d61b05d173797f51fced0efeca7a3ba1ee
gabriel padilha <gabriel@crpmango.com.br>
2024-06-24 12:13:02 -0300

    esse aqui

--- api/src/serve.ts
+++ api/src/serve.ts
@@ -4,14 +4,10 @@ import { routes } from './Routes/index';
 
 const app = express();
 
-// Configurar o middleware CORS
 app.use(cors());
 
-// Configurar o middleware para parsear JSON
 app.use(express.json());
 
-// Usar as rotas definidas 
 app.use(routes);
 
-// Iniciar o servidor
 app.listen(3333, () => console.log("Servidor rodando na porta 3333"));
